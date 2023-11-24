4eefe4c61827b00b41a3b7d5a4bf67ba684f4ad6
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2023-11-23 21:28:59 -0800

    Added clicking logic

--- src/TicTacToeGame/Square.jsx
+++ src/TicTacToeGame/Square.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const Square = () => {
+const Square = (props) => {
   return (
-    <div style={{ border: '1px solid', height: '100px', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}
-    className='square'>
-        <h5>X</h5>
+    <div
+      onClick={props.onClick}
+      style={{
+        border: '1px solid',
+        height: '100px',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      className='square'
+    >
+      <h5>{props.value}</h5>
     </div>
-  )
-}
+  );
+};
 
-export default Square
+export default Square;
